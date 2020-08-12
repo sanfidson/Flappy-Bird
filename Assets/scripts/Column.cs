0f58b88871a0921a77cb2e7d8c102bc535dd70b8
sanfidson <44056950+sanfidson@users.noreply.github.com>
2020-08-12 19:15:57 +0300

    Add files via upload
    First commit

---
+++ Assets/scripts/Column.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Column : MonoBehaviour
+{
+    public GameObject bird;
+    private int run;
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        if (bird)
+        {
+            run = 1;
+        } else 
+        {
+            run = 0;
+        }
+        transform.Translate((Vector2.left * speed * Time.deltaTime)*run);
+    }
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+    public float force;
+    public int points;
+    public int run = 1;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            rigidBody.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+        }
+    }
+    void RestartGame()
+    {
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        run = 0;
+        Debug.Log("You LOOOSE!!");
+        Destroy(gameObject);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        points++;
+        Debug.Log(points);
+    }
+}
